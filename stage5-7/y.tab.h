46460c02f000523d067fd6f8dbaa14af6f0ff7e7
Abhishek <abhi27051995@gmail.com>
2015-02-19 08:26:19 +0530

    Updated Stage5-7

--- stage5-7/y.tab.h
+++ stage5-7/y.tab.h
@@ -1,19 +1,19 @@
-/* A Bison parser, made by GNU Bison 2.5.  */
+/* A Bison parser, made by GNU Bison 3.0.2.  */
 
 /* Bison interface for Yacc-like parsers in C
-   
-      Copyright (C) 1984, 1989-1990, 2000-2011 Free Software Foundation, Inc.
-   
+
+   Copyright (C) 1984, 1989-1990, 2000-2013 Free Software Foundation, Inc.
+
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation, either version 3 of the License, or
    (at your option) any later version.
-   
+
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-   
+
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
@@ -26,38 +26,46 @@
    special exception, which will cause the skeleton and the resulting
    Bison output files to be licensed under the GNU General Public
    License without this special exception.
-   
+
    This special exception was added by the Free Software Foundation in
    version 2.2 of Bison.  */
 
+#ifndef YY_YY_Y_TAB_H_INCLUDED
+# define YY_YY_Y_TAB_H_INCLUDED
+/* Debug traces.  */
+#ifndef YYDEBUG
+# define YYDEBUG 0
+#endif
+#if YYDEBUG
+extern int yydebug;
+#endif
 
-/* Tokens.  */
+/* Token type.  */
 #ifndef YYTOKENTYPE
 # define YYTOKENTYPE
-   /* Put the tokens into the symbol table, so that GDB and other debuggers
-      know about them.  */
-   enum yytokentype {
-     NUM = 258,
-     ID = 259,
-     READ = 260,
-     WRITE = 261,
-     IF = 262,
-     THEN = 263,
-     ENDIF = 264,
-     WHILE = 265,
-     DO = 266,
-     ENDWHILE = 267,
-     INTEGER = 268,
-     DECL = 269,
-     ENDDECL = 270,
-     EQUAL = 271,
-     TRUE = 272,
-     FALSE = 273,
-     GE = 274,
-     LE = 275,
-     NE = 276,
-     BOOLEAN = 277
-   };
+  enum yytokentype
+  {
+    NUM = 258,
+    ID = 259,
+    READ = 260,
+    WRITE = 261,
+    IF = 262,
+    THEN = 263,
+    ENDIF = 264,
+    WHILE = 265,
+    DO = 266,
+    ENDWHILE = 267,
+    INTEGER = 268,
+    DECL = 269,
+    ENDDECL = 270,
+    EQUAL = 271,
+    TRUE = 272,
+    FALSE = 273,
+    GE = 274,
+    LE = 275,
+    NE = 276,
+    BOOLEAN = 277
+  };
 #endif
 /* Tokens.  */
 #define NUM 258
@@ -81,30 +89,26 @@
 #define NE 276
 #define BOOLEAN 277
 
-
-
-
+/* Value type.  */
 #if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
-typedef union YYSTYPE
+typedef union YYSTYPE YYSTYPE;
+union YYSTYPE
 {
-
-/* Line 2068 of yacc.c  */
-#line 10 "slp.y"
+#line 10 "slp.y" /* yacc.c:1909  */
 
 	int ival;
 	char *var;
 	struct tree_node *nptr;	
 
-
-
-/* Line 2068 of yacc.c  */
-#line 102 "y.tab.h"
-} YYSTYPE;
+#line 104 "y.tab.h" /* yacc.c:1909  */
+};
 # define YYSTYPE_IS_TRIVIAL 1
-# define yystype YYSTYPE /* obsolescent; will be withdrawn */
 # define YYSTYPE_IS_DECLARED 1
 #endif
 
+
 extern YYSTYPE yylval;
 
+int yyparse (void);
 
+#endif /* !YY_YY_Y_TAB_H_INCLUDED  */
